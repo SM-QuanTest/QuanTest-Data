dc06f6b4ce51d19ceb689366a6a83d00e8ba0211
sujeongmoon <msjk9000@gmail.com>
2025-08-25 18:57:10 +0900

    .env 로드 위치 변경

--- src/config/config.py
+++ src/config/config.py
@@ -2,11 +2,21 @@ import json
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-# config.py 파일 위치 기준으로 상위(또는 프로젝트) 루트의 .env 파일 로드
-BASE_DIR = Path(__file__).resolve().parent
-load_dotenv(BASE_DIR / ".env")
+env_path = find_dotenv(filename=".env", usecwd=True)
+
+if not env_path:
+    PROJ_ROOT = Path(__file__).resolve().parents[2]  # .../QuanTest-Data
+    candidate = PROJ_ROOT / ".env"
+    if candidate.exists():
+        env_path = str(candidate)
+
+if env_path:
+    load_dotenv(env_path)
+else:
+    pass
+
 
 DB_URL = os.getenv("DB_URL")
 # API_ENDPOINT = os.getenv("API_ENDPOINT")
